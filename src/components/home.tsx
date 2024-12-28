faf57971346d1d1e495a5e9b0c110fee83749130
Jakob Schulze <Jakob.Schulze1206@gmail.com>
2024-12-28 14:53:09 +0100

    Added GameCreation

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,36 +1,54 @@
-import TopBar from './CustomButton/TopBar';
-import GamesList from './dart/GameList';
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import TopBar from "./CustomButton/TopBar";
+import GamesList from "./dart/GameList";
+import { Button } from "@mui/joy";
+import { Add } from "@mui/icons-material";
+import pb from "../services/pocketbase";
 
 const Homepage = () => {
+  const [loading, SetLoading] = useState(false);
+
+  const newGame = async () => {
+    SetLoading(true);
+    const user = pb.authStore.record;
+    if (user) {
+      const throws = await pb.collection("Throws").create();
+
+      const game = await pb
+        .collection("Games")
+        .create({ owner: user.id, throws: throws.id });
+
+      console.log("Throws: ", throws);
+      console.log("Games: ", game);
+    }
+
+    SetLoading(false);
+  };
+
   return (
-    <div style={{ maxWidth: '1200px', margin: '0 auto', padding: '20px' }}>
+    <div style={{ maxWidth: "1200px", margin: "0 auto", padding: "20px" }}>
       <TopBar />
 
-      <header style={{ textAlign: 'center', margin: '20px 0' }}>
+      <header style={{ textAlign: "center", margin: "20px 0" }}>
         <h1>Welcome to Dart Counter</h1>
         <p>Track your dart games effortlessly!</p>
       </header>
 
-      <section style={{ margin: '20px 0' }}>
+      <section style={{ margin: "20px 0" }}>
         <h2>Start a New Game</h2>
-        <Link to="/dart" style={{ textDecoration: 'none' }}>
-          <button
-            style={{
-              padding: '10px 20px',
-              backgroundColor: '#4CAF50',
-              color: '#fff',
-              border: 'none',
-              borderRadius: '5px',
-              cursor: 'pointer',
-            }}
-          >
-            New Game
-          </button>
-        </Link>
+        {/*<Link to="/dart" style={{ textDecoration: 'none' }}> */}
+        <Button
+          loading={loading}
+          size="lg"
+          endDecorator={<Add />}
+          color="success"
+          onClick={() => newGame()}
+        >
+          New Game
+        </Button>
       </section>
 
-      <section style={{ margin: '20px 0' }}>
+      <section style={{ margin: "20px 0" }}>
         <h2>Past Games</h2>
         <GamesList />
       </section>
