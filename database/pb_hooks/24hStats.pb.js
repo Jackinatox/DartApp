3b75f883d92bd6975bae5f6ec3a03523c8e07848
Jackinatox <jakob.schulze1206@gmail.com>
2025-01-07 15:24:59 +0100

    Playcegolder for DailyStats with cron

---
+++ database/pb_hooks/24hStats.pb.js
@@ -0,0 +1,46 @@
+/// <reference path="../data/types.d.ts" />
+
+// cronAdd("test", "*/1 * * * *", async () => {
+//     console.log("executed every minute");
+
+//     pb.logger().info(`Starting event Handler`);
+
+//     try {
+//         const result = new DynamicModel({
+//             singles: 0,
+//             doubles: 0,
+//             triples: 0,
+//             misses: 0,
+//             total: 0,
+//         });
+
+//         await pb.db()
+//             .newQuery(`SELECT SUM(singles) as singles, sum(doubles) as doubles, SUM(triples) as triples, SUM(misses) as misses, SUM(singles) * 20 + sum(doubles) * 40 + SUM(triples) * 60 as total
+//                 FROM Throws`)
+//             .one(result);
+
+
+//         pb.logger().info(`Total Points: ${result.total}`);
+//         pb.logger().info(`Result object: ${JSON.stringify(result)}`);
+
+//         let allRecs = pb.findAllRecords("GlobalStats");   
+//         if(allRecs.length === 0) {
+//             pb.logger().warn("Creating new Stats Record");
+//             pb.logger().error("Creating new Stats Record Not implemented yet");
+//             // let record = new Record(pb).set('singles', result.singles).set('doubles', result.doubles).set('triples', result.triples).set('misses', result.misses).set('total', result.total);
+//             // pb.save(record);   
+//         } else {
+//             let record = allRecs[0];   
+//             pb.logger().info(`Editing Existring record, old: ${JSON.stringify(record)}`);
+//             record.set('TotalSingle', result.singles);
+//             record.set('TotalDouble', result.doubles);
+//             record.set('TotalTriple', result.triples);
+//             record.set('TotalMisses', result.misses);
+//             record.set('TotalPoints', result.total);   // Update it with new data
+//             pb.logger().info(`Editing Existring record, new: ${JSON.stringify(record)}`);
+//             pb.save(record);  
+//         }
+//     } catch (e) {
+//         pb.logger().error(`Error: ${e}`);
+//     }
+//   });
