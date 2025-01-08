8482759e9aab64272859af23d5077430495e106d
Jackinatox <jakob.schulze1206@gmail.com>
2025-01-08 17:06:32 +0100

    Proper Env Variable

--- src/services/pocketbase.ts
+++ src/services/pocketbase.ts
@@ -1,4 +1,4 @@
 import PocketBase from 'pocketbase';
 
-const pb = new PocketBase(process.env.REACT_APP_POCKETBASE_URL);
+const pb = new PocketBase(import.meta.env.VITE_POCKETBASE_URL);
 export default pb;
